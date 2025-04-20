90cba2fc747cce697ed23c2c43700f1ccef7d25d
malekgramadan <mintywicked@gmail.com>
2025-04-20 21:49:24 +0300

    Adding the quizzes to the localstorage in an array with ids to be able to be used by other scripts, also fixed typos in homepage.

--- scripts/auth.js
+++ scripts/auth.js
@@ -56,3 +56,72 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
     localStorage.setItem('currentUser', JSON.stringify(user));
     window.location.href = 'homepage.html';
 });
+
+if (!localStorage.getItem('quizzes')) {
+    const defaultQuizzes = [
+        {
+            id: 1,
+            title: "General Knowledge",
+            questions: [{
+                question: "What was the first movie ever to win the Academy Award for Best Picture?",
+                options: ["Gone Wild", "Sunrise", "Wings"],
+                answer: "Wings"
+            },
+            {
+                question: "Which planet is known as the Red Planet?",
+                options: ["Venus", "Mars", "Jupiter"],
+                answer: "Mars"
+            },
+            {
+                question: "What is the largest mammal?",
+                options: ["Elephant", "Blue Whale", "Giraffe"],
+                answer: "Blue Whale"
+            }
+        ]
+        },
+        {
+            id: 2,
+            title: "Science Quiz",
+            questions: [
+            {
+                question: "What is H2O?",
+                options: ["Gold", "Water", "Carbon Dioxide"],
+                answer: "Water"
+            },
+            {
+                question: "What is the chemical symbol for gold?",
+                options: ["Au", "Ag", "Go"],
+                answer: "Au"
+            },
+            {
+                question: "What force pulls objects toward Earth?",
+                options: ["Magnetism", "Gravity", "Friction"],
+                answer: "Gravity"
+            }
+            ]
+        },
+        {
+            id: 3,
+            title: "Geography Quiz",
+            questions: [
+                {
+                    question: "Which country has the largest population?",
+                    options: ["India", "China", "USA"],
+                    answer: "China"
+                },
+                {
+                    question: "What is the capital of Canada?",
+                    options: ["Toronto", "Ottawa", "Vancouver"],
+                    answer: "Ottawa"
+                },
+                {
+                    question: "How many states are in the United States of America?",
+                    options: ["52", "50", "48"],
+                    answer: "50"
+                }
+            ]
+        }
+        //You can add more questions but bewary of the duplicate IDs to avoid errors.
+    ];
+    localStorage.setItem('quizzes', JSON.stringify(defaultQuizzes));
+}
