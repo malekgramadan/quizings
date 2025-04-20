08497b6fe9a03bf67e6743086b12355f91c7624d
malekgramadan <mintywicked@gmail.com>
2025-04-21 00:25:12 +0300

    Importing the name of the user and displaying it beside the welcoming header.

--- scripts/hp.js
+++ scripts/hp.js
@@ -2,3 +2,9 @@ document.getElementById('logoutBtn').addEventListener('click', () => {
     localStorage.removeItem('currentUser');
     window.location.href = 'index.html';
 });
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+if (!currentUser) {
+    window.location.href = 'index.html';
+}
+
+document.getElementById('userName').textContent = currentUser.name ? `, ${currentUser.name}` : '';
