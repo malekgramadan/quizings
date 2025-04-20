7729ea6becddef2d2f27dfe4c37ee1b2b5ab445f
malekgramadan <mintywicked@gmail.com>
2025-04-20 19:43:28 +0300

    Adding a finding method that  checks wether there is already a user available or not, and if successful, it would redirect them to the homepage.

--- scripts/auth.js
+++ scripts/auth.js
@@ -35,6 +35,8 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
+    const users = JSON.parse(localStorage.getItem('users'));
+    const user = users.find(u => u.email === email && u.password === password);
     const adminUser = {
         name: 'Admin',
         email: 'admin@quiz.com',
@@ -47,4 +49,10 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
         window.location.href = 'dashboard.html';
         return;
     }
+    if (!user) {
+        document.getElementById('loginError').textContent = 'Invalid email or password!';
+        return;
+    }
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    window.location.href = 'homepage.html';
 });
