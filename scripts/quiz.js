a76e7b67f52b88d070f875157a500fc851afbfb8
malekgramadan <mintywicked@gmail.com>
2025-04-21 14:27:24 +0300

    Getting the current user and the quiz selected by the user from their respective pages, while using JSON to convert the data from strings to javascript object.

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,3 +1,11 @@
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+if (!currentUser) {
+    window.location.href = 'index.html';
+}
+const quiz = JSON.parse(localStorage.getItem('currentQuiz'));
+if (!quiz) {
+    window.location.href = 'homepage.html';
+}
 document.getElementById('backBtn').addEventListener('click', () => {
     window.location.href = 'homepage.html';
 });
