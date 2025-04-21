ef4b68909ea7c41d33d8a398789ab72ee36db4bf
malekgramadan <mintywicked@gmail.com>
2025-04-21 16:43:56 +0300

    Retrieving Data from localstorage for users and the quizzes.

--- scripts/db.js
+++ scripts/db.js
@@ -2,3 +2,5 @@ document.getElementById('logoutBtn').addEventListener('click', () => {
     localStorage.removeItem('currentUser');
     window.location.href = 'index.html';
 });
+const users = JSON.parse(localStorage.getItem('users')) || [];
+const quizzes = JSON.parse(localStorage.getItem('quizzes')) || [];
